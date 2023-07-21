c9fb23f6f5d7d2c1f224db6dd0dbbbc98bd6544e
vlrmzz <valerio.mazzone@syntheticus.ai>
2023-07-21 16:25:17 +0200

    Bumped to version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='syntheticus_connect',
-    version='0.1.5',
+    version='0.1.6',
     description='A Python client for Syntheticus',
     author='Valerio Mazzone',
     author_email='valerio.mazzone@gmail.com',

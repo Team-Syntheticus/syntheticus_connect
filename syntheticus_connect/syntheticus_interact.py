18190871e6942323d006237d17106ee572d78004
vlrmzz <valerio.mazzone@syntheticus.ai>
2023-11-24 19:46:13 +0100

    complete refactor.

--- syntheticus_connect/syntheticus_interact.py
+++ syntheticus_connect/syntheticus_interact.py
@@ -1,35 +1,36 @@
-from ipywidgets import widgets, Layout, VBox, Output
+from ipywidgets import widgets, Layout, Button, Text, Password, VBox, Output, Dropdown
 from IPython.display import display
+
 from syntheticus_connect import syntheticus_client
 from contextlib import contextmanager
 
-class syntheticus_interface(syntheticus_client):
+class syntheticus_interact(syntheticus_client):
     def __init__(self, host_django=None, host_airflow=None):
         super().__init__(host_django, host_airflow)
     
-    @contextmanager
-    def suppress_output(self):
-        with Output():
-            yield
+    # @contextmanager
+    # def suppress_output(self):
+    #     with Output():
+    #         yield
     
-    def login_user(self, _):
-        # Login widgets
-        self.login_username_input = widgets.Text(placeholder="Username")
-        self.login_password_input = widgets.Password(placeholder="Password")
-        self.login_button = widgets.Button(description="Login")
-        self.login_output = Output()  # Create an Output widget
-        
-        def on_login_button_click(_):
-            response = self.login(self.login_username_input.value, self.login_password_input.value)
-            with self.login_output:
-                print(response)
-        
-        self.login_button.on_click(on_login_button_click)
-        
-    def user_login(self):
-        self.login_user(None)  # Initialize the login widgets
-        display(self.login_username_input, self.login_password_input, self.login_button, self.login_output)
-    
+    def login_user(self):
+        username_input = Text(placeholder='Username')
+        password_input = Password(placeholder='Password')
+        login_button = Button(description='Login')
+        output_area = Output()
+
+        def on_login_button_click(b):
+            self.user = str(username_input.value)
+            self.password = str(password_input.value)
+            # Call the login method of syntheticus_client and display the response
+            with output_area:
+                response = self.login(self.user, self.password)
+                print(response)  # This will output to the output_area widget
+
+        login_button.on_click(on_login_button_click)
+
+        display(VBox([username_input, password_input, login_button, output_area]))
+
     def project_select(self):
         """
         Create a project selection dropdown widget.
@@ -44,28 +45,22 @@ class syntheticus_interface(syntheticus_client):
         project_strings = [f"ID: {project['id']}, Name: {project['name']}"
                            for project in unique_projects] or ["No projects available"]
 
-        # Check if the widget already exists
+        # Create or update the Dropdown widget
         if hasattr(self, 'project_dropdown_widget'):
-            # Update the options of the existing dropdown widget
             self.project_dropdown_widget.options = project_strings
             self.project_dropdown_widget.value = project_strings[0] if project_strings else None
         else:
-            # Create the Dropdown widget
-            self.project_dropdown_widget = widgets.Dropdown(
+            self.project_dropdown_widget = Dropdown(
                 options=project_strings,
                 value=project_strings[0] if project_strings else None,
                 description='Select:',
                 disabled=not bool(project_strings),
                 layout=Layout(width='auto')
             )
-            # Display the widget for the first time
-            display(self.project_dropdown_widget)
 
-        # Check if the output area exists
+        # Create or update the Output area
         if not hasattr(self, 'project_output_area'):
             self.project_output_area = Output()
-            display(self.project_output_area)
-        # this need to selt self.project_name    
 
         def update_variables(change):
             """
@@ -99,27 +94,34 @@ class syntheticus_interface(syntheticus_client):
         if len(project_strings) == 1:
             update_variables({'new': self.project_dropdown_widget.value})
 
-        # Display the Dropdown widget and the output area
+        # Display the Dropdown widget and the output area only once
         display(VBox([self.project_dropdown_widget, self.project_output_area]))
 
     def dataset_select(self):
-        with self.suppress_output():
-            self.get_datasets()
+        self.get_datasets()
 
-        transformed_table_data = []
-        for dataset in self.table_data:
-            transformed_table_data.append({
-                'dataset_name': dataset[0],
-                'dataset_id': dataset[1],
-                'project_id': dataset[2],
-                'data_type': dataset[3],
-                'size': dataset[4],
-                'rows_number': dataset[5],
-                'num_columns': dataset[6],
-                'status': dataset[7]
-            })
-        
-        dataset_strings = [f"ID: {entry['dataset_id']}, Name: {entry['dataset_name']}" for entry in transformed_table_data]
+        # Group datasets by ID and count the number of files/tables per ID
+        datasets_by_id = {}
+        for result in self.data.get('results', []):
+            for outer_dataset in result.get('datasets', []):
+                for dataset in outer_dataset.get('datasets', []):
+                    dataset_id = result.get('id')
+                    if dataset_id not in datasets_by_id:
+                        datasets_by_id[dataset_id] = {
+                            'count': 0,
+                            'name': dataset.get('dataset_name'),
+                            'project_id': result.get('project'),
+                            'data_type': result.get('data_type'),
+                            'size': dataset.get('size'),
+                            'rows_number': dataset.get('rows_number'),
+                            'num_columns': len(dataset.get('dataset_metadata', {}).get('column_types', {})),
+                            'status': 'Selected' if self.dataset_id == dataset_id else 'Not Selected'
+                        }
+                    datasets_by_id[dataset_id]['count'] += 1
+
+        # Prepare dataset strings for the dropdown
+        dataset_strings = [f"ID: {dataset_id}, Name: {info['name']} (# of tables: {info['count']})"
+                           for dataset_id, info in datasets_by_id.items()]
         dataset_strings.insert(0, "Select a dataset")
 
         self.dataset_dropdown_widget = widgets.Dropdown(
@@ -134,23 +136,24 @@ class syntheticus_interface(syntheticus_client):
 
         def update_variables(change):
             selected_value = change['new']
-            selected_index = dataset_strings.index(selected_value)
-            selected_index = selected_index - 1
+            selected_index = dataset_strings.index(selected_value) - 1  # Adjust for 'Select a dataset'
 
             if selected_index >= 0:
-                selected_dataset = transformed_table_data[selected_index]
-                self.dataset_id = selected_dataset['dataset_id']
-                self.dataset_name = selected_dataset['dataset_name']
+                selected_dataset_id = list(datasets_by_id.keys())[selected_index]
+                selected_dataset_info = datasets_by_id[selected_dataset_id]
+
+                self.dataset_id = selected_dataset_id
+                self.dataset_name = selected_dataset_info['name']
 
                 with self.dataset_output_area:
                     self.dataset_output_area.clear_output()
                     print(f"Selected dataset ID: {self.dataset_id}")
                     print(f"Selected dataset Name: {self.dataset_name}")
-                    print(f"Project ID: {selected_dataset['project_id']}")
-                    print(f"Data Type: {selected_dataset['data_type']}")
-                    print(f"Size: {selected_dataset['size']}")
-                    print(f"Number of Rows: {selected_dataset['rows_number']}")
-                    print(f"Number of Columns: {selected_dataset['num_columns']}")
+                    print(f"Project ID: {selected_dataset_info['project_id']}")
+                    print(f"Data Type: {selected_dataset_info['data_type']}")
+                    print(f"Size: {selected_dataset_info['size']}")
+                    print(f"Number of Rows: {selected_dataset_info['rows_number']}")
+                    print(f"Number of Columns: {selected_dataset_info['num_columns']}")
                     print("\n")
             else:
                 with self.dataset_output_area:
@@ -161,8 +164,8 @@ class syntheticus_interface(syntheticus_client):
         display(VBox([self.dataset_dropdown_widget, self.dataset_output_area]))
 
     def model_select(self):
-        with self.suppress_output():
-            self.get_models()
+
+        self.get_models()
 
         models_data = []
         for model in self.models:
@@ -205,16 +208,14 @@ class syntheticus_interface(syntheticus_client):
 
         display(VBox([self.model_dropdown_widget, self.model_output_area]))
         
-    def commit_select(self):
+    def experiment_select(self):
         """
         Displays a dropdown widget for commit selection.
 
         Returns:
             None
         """
-        
-        with self.suppress_output():
-            self.list_commits()  # Fetch commits data
+        self.get_commits()  # Fetch commits data
 
         commit_strings = [f"ID: {entry['commit']}, Message: {entry['subject']}" for entry in self.commits]
         commit_strings.insert(0, "Select a commit")

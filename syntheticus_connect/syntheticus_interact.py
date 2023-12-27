73c57bf7fd9f8084df2498073b0ce45c4282175d
vlrmzz <valerio.mazzone@syntheticus.ai>
2023-12-27 16:25:07 +0100

    WIP: Added basic widget for configuration file

--- syntheticus_connect/syntheticus_interact.py
+++ syntheticus_connect/syntheticus_interact.py
@@ -3,6 +3,8 @@ from IPython.display import display
 
 from syntheticus_connect import syntheticus_client
 from contextlib import contextmanager
+import yaml
+import requests
 
 class syntheticus_interact(syntheticus_client):
     def __init__(self, host_django=None, host_airflow=None):
@@ -30,72 +32,6 @@ class syntheticus_interact(syntheticus_client):
         login_button.on_click(on_login_button_click)
 
         display(VBox([username_input, password_input, login_button, output_area]))
-
-    # def project_select(self):
-    #     """
-    #     Create a project selection dropdown widget.
-    #     """
-    #     # Fetch project data
-    #     self.get_projects()
-
-    #     # Remove duplicate projects if any (based on project ID)
-    #     unique_projects = {proj['id']: proj for proj in self.projects_data}.values()
-
-    #     # Prepare project strings for the dropdown
-    #     project_strings = [f"ID: {project['id']}, Name: {project['name']}"
-    #                        for project in unique_projects] or ["No projects available"]
-
-    #     # Create or update the Dropdown widget
-    #     if hasattr(self, 'project_dropdown_widget'):
-    #         self.project_dropdown_widget.options = project_strings
-    #         self.project_dropdown_widget.value = project_strings[0] if project_strings else None
-    #     else:
-    #         self.project_dropdown_widget = Dropdown(
-    #             options=project_strings,
-    #             value=project_strings[0] if project_strings else None,
-    #             description='Select:',
-    #             disabled=not bool(project_strings),
-    #             layout=Layout(width='auto')
-    #         )
-
-    #     # Create or update the Output area
-    #     if not hasattr(self, 'project_output_area'):
-    #         self.project_output_area = Output()
-
-    #     def update_variables(change):
-    #         """
-    #         Update variables based on the selected dropdown value.
-
-    #         This function is called whenever the dropdown value changes.
-    #         It updates the selected project's information and displays it.
-
-    #         Args:
-    #             change (dict): A dictionary containing information about the change.
-
-    #         Returns:
-    #             None
-    #         """
-    #         selected_value = change['new']
-    #         selected_index = project_strings.index(selected_value)
-    #         selected_project = self.projects_data[selected_index]
-    #         self.project_id = selected_project['id']
-    #         self.project_name = selected_project['name']
-
-    #         # Clear previous output and display the new project info
-    #         with self.project_output_area:
-    #             self.project_output_area.clear_output()
-    #             print(f"Selected Project ID: {self.project_id}")
-    #             print(f"Selected Project Name: {self.project_name}")
-
-    #     # Attach the update function to the dropdown's 'value' property changes
-    #     self.project_dropdown_widget.observe(update_variables, names='value')
-
-    #     # Check if there's only one project in the dropdown
-    #     if len(project_strings) == 1:
-    #         update_variables({'new': self.project_dropdown_widget.value})
-
-    #     # Display the Dropdown widget and the output area only once
-    #     display(VBox([self.project_dropdown_widget, self.project_output_area]))
 
     def project_select(self):
         """
@@ -235,6 +171,109 @@ class syntheticus_interact(syntheticus_client):
 
         display(VBox([self.dataset_dropdown_widget, self.dataset_output_area]))
 
+    def create_config_file(self):
+        # Widget definitions
+        transform_table_name_widget = widgets.Text(description='Table Name:', value='')
+        transform_type_widget = widgets.Dropdown(
+            description='Type:',
+            options=['drop', 'fake', 'hash', 'number_shift', 'date_shift', 'generalize', 'shuffle', 'k_anonymity', 'l_diversity', 't_closeness', 'perturbation', 'tokenization'],
+            value='drop')
+        transform_columns_widget = widgets.Textarea(description='Columns:', value='')
+
+        model_type_widget = widgets.Text(description='Model Type:', value='')
+
+        sample_number_rows_widget = widgets.IntText(description='Number of Rows:', value=100)
+
+        metrics_table_name_widget = widgets.Text(description='Table Name:', value='')
+        metrics_column_plot_widget = widgets.Textarea(description='Column Plot:', value='')
+
+        # Organize widgets into sections
+        transform_section_widgets = VBox([transform_table_name_widget, transform_type_widget, transform_columns_widget])
+        model_section_widgets = VBox([model_type_widget])
+        sample_section_widgets = VBox([sample_number_rows_widget])
+        metrics_section_widgets = VBox([metrics_table_name_widget, metrics_column_plot_widget])
+
+        # Accordion to hold sections
+        accordion = widgets.Accordion(children=[transform_section_widgets, model_section_widgets, sample_section_widgets, metrics_section_widgets])
+        accordion.set_title(0, 'Transform')
+        accordion.set_title(1, 'Model')
+        accordion.set_title(2, 'Sample')
+        accordion.set_title(3, 'Metrics')
+
+        # Function to Generate Configuration
+        def generate_config():
+            config_yaml_lines = [
+                "config_version: '1.0'",
+                "config_name: base",
+                "config_steps:"
+            ]
+
+            # Add 'data' section
+            config_yaml_lines.append("  - data:")
+            config_yaml_lines.append(f"      dataset_version: {self.dataset_id}")
+
+            # Add 'transform' section
+            if transform_table_name_widget.value or transform_columns_widget.value:
+                # Assuming some logic to format transform section
+                pass
+            else:
+                config_yaml_lines.append("  - transform:")
+
+            # Add 'model' section
+            if model_type_widget.value:
+                # Assuming some logic to format model section
+                pass
+            else:
+                config_yaml_lines.append("  - model:")
+
+            # Add 'sample' section
+            if sample_number_rows_widget.value:
+                config_yaml_lines.append("  - sample:")
+                config_yaml_lines.append(f"      number_of_rows: {sample_number_rows_widget.value}")
+            else:
+                config_yaml_lines.append("  - sample:")
+
+            # Add 'metrics' section
+            if metrics_table_name_widget.value or metrics_column_plot_widget.value:
+                # Assuming some logic to format metrics section
+                pass
+            else:
+                config_yaml_lines.append("  - metrics:")
+
+            return "\n".join(config_yaml_lines)
+
+        # Function to Handle Submit Action
+        def on_submit_clicked(b):
+            config_yaml = generate_config()
+
+            # Save the generated configuration to a file
+            self.config_file_path = f"{self.project_name}.yaml"
+            with open(self.config_file_path, 'w') as file:
+                file.write(config_yaml)
+            
+            # Now call upload_conf to upload the file
+            self.upload_gen_conf()
+
+        submit_button = Button(description="Create Config")
+        submit_button.on_click(on_submit_clicked)
+
+        # Display the Widget
+        display(accordion, submit_button)
+
+    def upload_gen_conf(self):
+        url_upload_conf = f'{self.host_django}/api/projects/{self.project_id}/update-conf-file/'
+
+        # Read the saved YAML file and upload it
+        with open(self.config_file_path, 'rb') as file:
+            files = [('file', (self.config_file_path, file, 'text/yaml'))]
+            headers = {'Authorization': f'Token {self.token}'}
+            response = requests.request("POST", url_upload_conf, headers=headers, files=files)
+
+        if response.status_code == 200:
+            print(f"A configuration file has been uploaded for the project {self.project_id}.")
+        else:
+            print(f"Error occurred while uploading the configuration file: STATUS CODE {response.status_code}")
+    
     def model_select(self):
 
         self.get_models()
